0f69e97f6cefb1d8330b30b30e9868a37c957076
David MacKillop <davidjmackillop@gmail.com>
2026-05-15 11:12:35 -0400

    feat: add database_name input and update defaults
    - Add database_name workflow input to deploy and validate (default: n8n)
    - Change default region from us-south1 to us-central1
    - Change default min_instances from 0 to 1
    - Update terraform/variables.tf region default to match

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "region" {
   description = "GCP region for Cloud Run and Cloud SQL."
   type        = string
-  default     = "us-south1"
+  default     = "us-central1"
 }
 
 variable "service_name" {
